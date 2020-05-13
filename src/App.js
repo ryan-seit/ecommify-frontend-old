36a9ab9a753acbd948910de3b2376964ded41f78
Ryan Seit <ryan@ryanseit.com>
2020-05-13 15:44:46 -0400

    built out login and signup logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
 
-function App() {
-  return (
-    <div>
+export default class App extends Component {
 
-    </div>
-  );
+  state = {
+    isLoggedIn: false,
+    user: {}
+  }
+
+  componentDidMount() {
+    this.loginStatus()
+  }
+
+  loginStatus = () => {
+    // allows Rails server to set and read the cookie on the front-ends browser
+    axios.get('http://localhost:3001/logged_in', {withCredentials: true})
+    .then(response => {
+      if (response.data.logged_in) {
+        this.handleLogin(response)
+      } else {
+        this.handleLogout()
+      }
+    })
+    .catch(error => console.log('api errors:', error))
+  };
+
+  handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user
+    })
+  };
+
+  handleLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+      user: {}
+    })
+  };
+
+  render () {
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route 
+              exact path='/'
+              render={props => (
+                <Home {...props} handleLogout={this.handleLogout} loggedInStatus={this.state.isLoggedIn}/>
+              )} 
+            />
+            <Route 
+              exact path='/login'
+              render={props => (
+                <Login {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn}/>
+              )}
+            />
+            <Route 
+              exact path='/signup' 
+              render={props => (
+                <Signup {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn}/>
+              )}
+            />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
-
-export default App;
